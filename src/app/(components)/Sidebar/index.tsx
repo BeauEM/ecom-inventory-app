9cf39c9cee69cb3362ba094053336346e0b45b60
BeauEM <beau.d.elliott1@gmail.com>
2024-08-20 16:13:39 -0600

    Create sidebar component

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,13 +1,30 @@
+"use client";
+import { Menu } from "lucide-react";
 import React from "react";
 
 const Sidebar = () => {
   return (
     <div>
+      {/* Top logo */}
       <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
         <div>Logo</div>
         <h1 className="font-extrabold text-2xl">BEESTOCK</h1>
+
+        <button
+          className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={() => {}}
+        >
+          <Menu className="w-4 h-4" />
+        </button>
       </div>
-      <button className=""></button>
+      {/* LINKS */}
+      <div className="flex-grow mt-8">{/* LINKS HERE */}</div>
+      {/* FOOTER */}
+      <div>
+        <p className="text-center text-xs text-gray-500">
+          &copy; 2024 BEESTOCK
+        </p>
+      </div>
     </div>
   );
 };
