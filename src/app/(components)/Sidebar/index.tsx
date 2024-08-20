c316f388e93c8806e7311e5046adbc50631bfdcf
BeauEM <beau.d.elliott1@gmail.com>
2024-08-20 13:33:59 -0600

    Build Navbar component

---
+++ src/app/(components)/Sidebar/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+        <div>Logo</div>
+        <h1 className="font-extrabold text-2xl">BEESTOCK</h1>
+      </div>
+      <button className=""></button>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Navbar from "@/app/(components)/NavBar";
+import Sidebar from "@/app/(components)/Sidebar";
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
-      Sidebar
-      <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50`}>
+      <Sidebar />
+      <main
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}
+      >
+        <Navbar />
         {children}
       </main>
     </div>
