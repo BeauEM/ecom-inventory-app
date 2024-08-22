d5b87b740229addef7eebdfe349aa7237496e160
BeauEM <beau.d.elliott1@gmail.com>
2024-08-22 13:53:25 -0600

    Enable sidebar collapse

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,10 +1,27 @@
 "use client";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/state";
 import { Menu } from "lucide-react";
 import React from "react";
 
 const Sidebar = () => {
+  //add state
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
+  //apply diff settings to sidebar if collapsed or !
+  const sidebarClassnames = `fixed flex flex-col ${
+    isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
+  } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
+
   return (
-    <div>
+    <div className={sidebarClassnames}>
       {/* Top logo */}
       <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
         <div>Logo</div>
@@ -12,7 +29,7 @@ const Sidebar = () => {
 
         <button
           className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-          onClick={() => {}}
+          onClick={toggleSidebar}
         >
           <Menu className="w-4 h-4" />
         </button>
