d778f55a3678c0e817b4253d2943d696ccd982d7
BeauEM <beau.d.elliott1@gmail.com>
2024-08-24 12:26:38 -0600

    Create dashboard controller to handle data from sql tables

---
+++ server/dist/src/controllers/dashboardController.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+//5 different diff data sources from sql tables
+const prisma = new PrismaClient();
+
+export const getDashboardMetrics = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const popularProducts = await prisma.products.findMany({
+      take: 15,
+      orderBy: {
+        stockQuantity: "desc",
+      },
+    });
+    const saleSummary = await prisma.salesSummary.findMany({
+      take: 5,
+      orderBy: {
+        date: "desc",
+      },
+    });
+    const purchaseSummary = await prisma.purchaseSummary.findMany({
+      take: 5,
+      orderBy: {
+        date: "desc",
+      },
+    });
+    const expenseSummary = await prisma.expenseSummary.findMany({
+      take: 5,
+      orderBy: {
+        date: "desc",
+      },
+    });
+    //save as raw so we can modify data returned
+    const expenseByCategorySummaryRaw = await prisma.expenseByCategory.findMany(
+      {
+        take: 5,
+        orderBy: {
+          date: "desc",
+        },
+      }
+    );
+    //map expenses to new obj, but with amt as a string
+    const expenseByCategorySummary = expenseByCategorySummaryRaw.map(
+      (item) => ({
+        ...item,
+        amount: item.amount.toString(),
+      })
+    );
+
+    res.json({
+      popularProducts,
+      saleSummary,
+      purchaseSummary,
+      expenseSummary,
+      expenseByCategorySummary,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving dashboard metrics" });
+  }
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -17,6 +17,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 //ROUTES
+app.get("/hello", (req, res) => {
+  res.send("hello world");
+});
 
 //SERVER
 const port = process.env.PORT || 3001;
